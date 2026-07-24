dbf84e191bef9c6020e4fe84860765cc4c9152e6
Eshan Kumar Jain <80900722+Eshan-kumar-jain@users.noreply.github.com>
2026-07-24 02:04:39 +0100

    Show patient_id, attending_doctor_id, and diagnosis for admissions that match one of the two criteria: 1. patient_id is an odd number and attending_doctor_id is either 1, 5, or 19. 2. attending_doctor_id contains a 2 and the length of patient_id is 3 characters.

---
+++ sql/2026-07-24-Show patient_id, attending_doctor_id, and diagnosis for admissions that match one of the two criteria: 1. patient_id is an odd number and attending_doctor_id is either 1, 5, or 19. 2. attending_doctor_id contains a 2 and the length of patient_id is 3 characters.sql
@@ -0,0 +1,3 @@
+select patient_id,attending_doctor_id,diagnosis from admissions where 
+(patient_id % 2 != 0 and attending_doctor_id in (1,5,19)) or 
+(attending_doctor_id LIKE '%2%' and len(patient_id) = 3);
