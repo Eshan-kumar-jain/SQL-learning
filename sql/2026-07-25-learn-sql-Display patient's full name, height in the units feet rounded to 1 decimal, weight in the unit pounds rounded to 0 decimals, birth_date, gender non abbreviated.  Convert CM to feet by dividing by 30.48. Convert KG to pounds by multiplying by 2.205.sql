12cea68cd7dd623c6a8f1c4d51d8ec647ad266f0
Eshan Kumar Jain <80900722+Eshan-kumar-jain@users.noreply.github.com>
2026-07-25 02:08:53 +0100

    Fix weight conversion and height calculation in SQL query

--- sql/2026-07-25-learn-sql-Display patient's full name, height in the units feet rounded to 1 decimal, weight in the unit pounds rounded to 0 decimals, birth_date, gender non abbreviated.  Convert CM to feet by dividing by 30.48. Convert KG to pounds by multiplying by 2.205.sql
+++ sql/2026-07-25-learn-sql-Display patient's full name, height in the units feet rounded to 1 decimal, weight in the unit pounds rounded to 0 decimals, birth_date, gender non abbreviated.  Convert CM to feet by dividing by 30.48. Convert KG to pounds by multiplying by 2.205.sql
@@ -1,5 +1,5 @@
 SELECT concat(first_name,' ',last_name) as patient_name, round(height/30.38,1) as height_in_feet,
-round(weight/2.205,0) as weight_in_kg , birth_date, 
+round(weight*2.205,0) as weight_in_kg , birth_date, 
 case 
 when gender = 'M' then 'MALE'
 else 'FEMALE'
