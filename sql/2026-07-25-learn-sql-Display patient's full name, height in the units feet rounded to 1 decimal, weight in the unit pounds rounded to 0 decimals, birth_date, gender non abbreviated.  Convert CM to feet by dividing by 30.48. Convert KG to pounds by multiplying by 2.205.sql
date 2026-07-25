8778e56c439da106a647295d2c5d85e07d2bb2d3
Eshan Kumar Jain <80900722+Eshan-kumar-jain@users.noreply.github.com>
2026-07-25 02:06:23 +0100

    Display patient's full name, height in the units feet rounded to 1 decimal, weight in the unit pounds rounded to 0 decimals, birth_date, gender non abbreviated.  Convert CM to feet by dividing by 30.48. Convert KG to pounds by multiplying by 2.205.

---
+++ sql/2026-07-25-learn-sql-Display patient's full name, height in the units feet rounded to 1 decimal, weight in the unit pounds rounded to 0 decimals, birth_date, gender non abbreviated.  Convert CM to feet by dividing by 30.48. Convert KG to pounds by multiplying by 2.205.sql
@@ -0,0 +1,7 @@
+SELECT concat(first_name,' ',last_name) as patient_name, round(height/30.38,1) as height_in_feet,
+round(weight/2.205,0) as weight_in_kg , birth_date, 
+case 
+when gender = 'M' then 'MALE'
+else 'FEMALE'
+END as gender_type; 
+from patients;
